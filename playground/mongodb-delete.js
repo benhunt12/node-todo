b1aae6b9cd9245b5e1e362763ac7beec949b1d9b
Dev Aspiricx <devaspiricx@Devs-Mac-mini.local>
2018-09-11 09:33:09 -0700

    more delete

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -7,12 +7,30 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     const db = client.db('TodoApp');
 
     // delete many
-    db.collection('Todos').deleteMany({text: 'eat lunch'}).then((res) => {
-        console.log(res);
-    })
-    // delete one
+    // db.collection('Todos').deleteMany({text: 'eat lunch'}).then((res) => {
+    //     console.log(res);
+    // });
 
-    // find one and delete
+    // delete one
+    // db.collection('Todos').deleteOne({text: 'eat lunch'}).then((res) => {
+    //     console.log(res);
+    // });
+
+    // // find one and delete
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((res) => {
+    //     console.log(res);
+    // });
+
+
+    // // delete all names with mike
+    // db.collection('Users').deleteMany({name: 'Mike'}).then((res) => {
+    //     console.log(res);
+    // });
+
+    // delete Jen by ID
+    db.collection('Users').deleteOne({_id: new ObjectID('5b96d01a2331b32b4fdec04f')}).then((res) => {
+        console.log(res);
+    });
 
     client.close();
 });

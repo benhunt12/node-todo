a9210234f0c18704cecb582afe794fa7640526cd
Dev Aspiricx <devaspiricx@Devs-Mac-mini.local>
2018-09-12 08:56:51 -0700

    add post todo route

---
+++ server/server.js
@@ -0,0 +1,31 @@
+// express for server
+var express = require('express');
+
+// JSON formatting
+var bodyParser = require('body-parser');
+
+// mongoose connection, models
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+// express application
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc)
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000!');
+})
